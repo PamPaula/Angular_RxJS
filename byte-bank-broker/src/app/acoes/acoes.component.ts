62ceeed35a4fc615b80d00f37b8a65d71e6af359
PamPaula <pamela.dpaulas@gmail.com>
2021-10-25 10:53:24 -0300

    estudos sobre angular e RxJs

---
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -0,0 +1,39 @@
+import { merge, Subscription } from 'rxjs';
+import { AcoesService } from './acoes.service';
+import { Acoes } from './modelo/acoes';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent {
+  acoesInput = new FormControl();
+  todaAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {
+      console.log('Fluxo Inicial');
+    })
+  );
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
+    tap(() => {
+      console.log('Fluxo de filtro');
+    }),
+    tap(console.log),
+    filter(
+      (valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length
+    ),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+
+  acoes$ = merge(this.todaAcoes$, this.filtroPeloInput$);
+
+  constructor(private acoesService: AcoesService) {}
+
+}
